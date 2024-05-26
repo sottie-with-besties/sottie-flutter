a296ac1308e80f6d58a527996a10ccd5967ef3c8
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-05-26 16:58:33 +0900

    클린 아키텍처

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sottie_flutter/main.dart';
+import 'package:sottie_flutter/core/main/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
