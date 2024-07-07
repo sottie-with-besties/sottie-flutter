d429f65fd72acc200efe3c77a2f78aabb749f05d
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-07-07 15:43:23 +0900

    dm ui

--- lib/ui/dm/widget/dm_chat_room.dart
+++ lib/ui/dm/widget/dm_chat_room.dart
@@ -18,44 +18,79 @@ class DmChatRoom extends StatelessWidget {
       color: mainSilverColor,
       child: InkWell(
         onTap: () {},
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Container(
-                padding: const EdgeInsets.all(12),
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
                   children: [
                     RandomAvatar(
                       DateTime.now().toIso8601String(),
                       width: 50.h,
                       height: 50.h,
                     ),
-                    SizedBox(
-                      width: 240.w,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            model.name,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 16.sp,
-                              fontWeight: FontWeight.bold,
-                            ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          model.name,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(model.latestMsg),
-                        ],
-                      ),
-                    )
+                        ),
+                        Text(model.latestMsg),
+                      ],
+                    ),
                   ],
                 ),
-              ),
+                IntrinsicHeight(
+                  child: SizedBox(
+                    width: 65.w,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          model.latestTime,
+                          style: const TextStyle(
+                            color: Colors.black54,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: 50.w,
+                          height: 20.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.redAccent.withOpacity(0.8),
+                          ),
+                          child: Center(
+                            child: Text(
+                              model.notReadMsg.toString(),
+                              style: TextStyle(
+                                color: mainSilverColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
