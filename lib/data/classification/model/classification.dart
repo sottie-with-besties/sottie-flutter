70d7e0f33edc80a5675f3faab4cfac01f6cfe5a9
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-07-10 01:30:33 +0900

    classification 필드값 추가
    채팅방 만들 때, 검색할 때 필요하다고 생각하는 필드값들 추가

--- lib/data/classification/model/classification.dart
+++ lib/data/classification/model/classification.dart
@@ -3,25 +3,31 @@ import 'package:sottie_flutter/data/classification/model/gender_restrictions.dar
 import 'package:sottie_flutter/data/classification/model/location.dart';
 
 class Classification {
-  List<Category> category;
+  List<Category> category; // 분류
   DateTime? date; // 시간도 포함, non-null 타입으로 안됨
-  Location location;
-  GenderRestrictions gender;
-  int minNumOfMember;
-  int maxNumOfMember;
-  int minAge;
-  int maxAge;
-  double manner;
+  Location location; // 지역
+  GenderRestrictions gender; // 성별 제한
+  int minNumOfMember; // 최소 참여자 수 제한
+  int maxNumOfMember; // 최대 참여자 수 제한
+  int minAge; // 최소 나이 제한
+  int maxAge; // 최대 나이 제한
+  double manner; // 사용자의 매너 점수, 매너 온도 제한
+  bool startSameTime; // 인원이 한번에 모였을 때 시작?
+  int startNumOfMember; // 몇명이 모였을 때 시작할 것인지? minAge <= n <= maxAge, startSameTime이 true일 때만.
+  bool openParticipation; // 채팅방이 생성된 후에도 도중에 누군가 들어올 수 있음.
 
   Classification({
     this.category = const [Category.others],
     this.date, // date와 time은 null로 못받게 프론트에서 예외 처리
     this.location = Location.seoul,
-    this.gender = GenderRestrictions.all, // null => 성별 상관없음
-    this.minNumOfMember = 0, // 0 => 최소 참여자 수 상관없음
-    this.maxNumOfMember = 0, // 0 => 최대 참여자 수 상관없음
-    this.minAge = 0, // 0 => 최소나이 상관없음
-    this.maxAge = 0, // 0 => 최대나이 상관없음
-    this.manner = 0, // null => 매너온도 상관없음
+    this.gender = GenderRestrictions.all, // all => 성별 상관 없음
+    this.minNumOfMember = 0, // 0 => 최소 참여자 수 상관 없음
+    this.maxNumOfMember = 0, // 0 => 최대 참여자 수 상관 없음
+    this.minAge = 0, // 0 => 최소 나이 상관 없음
+    this.maxAge = 0, // 0 => 최대 나이 상관 없음
+    this.manner = 0, // 0 => 매너 온도 상관 없음
+    this.startSameTime = false,
+    this.startNumOfMember = 0, // 0 => startSameTime false일때, 기본값은 maxAge
+    this.openParticipation = false,
   });
 }
