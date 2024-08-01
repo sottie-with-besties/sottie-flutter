75fbe0c03b14c250974122188c916bf644dd55c9
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-08-01 17:43:14 +0900

    이벤트 스크린

--- lib/ui/more/screen/extra_services/extra_event_screen.dart
+++ lib/ui/more/screen/extra_services/extra_event_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class ExtraEventScreen extends StatelessWidget {
@@ -9,10 +11,68 @@ class ExtraEventScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("이벤트"),
       ),
-      body: Column(
-        children: [
-          Text("Event"),
-        ],
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            _EventFeed(title: "이벤트1", subTitle: "2024-08-01 ~ 2024-09-01"),
+            _EventFeed(title: "이벤트2", subTitle: "2024-08-01 ~ 2024-09-01"),
+            _EventFeed(title: "이벤트3", subTitle: "2024-08-01 ~ 2024-09-01"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _EventFeed extends StatelessWidget {
+  const _EventFeed({
+    required this.title,
+    required this.subTitle,
+  });
+
+  final String title;
+  final String subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        log("이벤트");
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              height: 100,
+              child: const Center(
+                child: Text(
+                  "이벤트 이미지",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              subTitle,
+              style: const TextStyle(
+                fontSize: 11,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
