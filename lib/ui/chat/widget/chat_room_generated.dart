a274f6c76e520a9d3219d953e6698aff3d42bd28
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2024-10-05 18:47:44 +0900

    채팅 끝난 후 타이머 작동

--- lib/ui/chat/widget/chat_room_generated.dart
+++ lib/ui/chat/widget/chat_room_generated.dart
@@ -13,46 +13,13 @@ import 'package:sottie_flutter/ui/common/controller/screen_size.dart';
 import 'package:sottie_flutter/ui/common/widget/on_long_press_option.dart';
 import 'package:sottie_flutter/ui/common/widget/slide_tap_widget.dart';
 
-class ChatRoomGenerated extends StatefulWidget {
+class ChatRoomGenerated extends StatelessWidget {
   const ChatRoomGenerated({
     super.key,
     required this.model,
   });
 
   final ChatRoomModel model;
-
-  @override
-  State<ChatRoomGenerated> createState() => _ChatRoomGeneratedState();
-}
-
-class _ChatRoomGeneratedState extends State<ChatRoomGenerated> {
-  bool chattingOver = false;
-  Duration chatRoomDisappearingTime = const Duration(hours: 24);
-
-  void _alarmOnOffAction(bool withSlide) {
-    log("DmAction");
-  }
-
-  void _chatRoomOutAction(bool withSlide) {
-    log("DeleteAction");
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    /// 채팅 모임 날짜 기준 24시간 경과 후 채팅방이 사라지기 시작함. 24시간 후 완전히 사라짐.
-    /// 모임 날짜 + 24시간 까지 채팅 가능, 그 이후 24시간 채팅방 삭제 대기
-    /// inDays == 1은 시간 차이가 24시간 이상 48시간 미만을 의미
-    final generatedDate = DateTime.parse(widget.model.date);
-    final now = DateTime.now();
-    final du = now.difference(generatedDate);
-    if (du.inDays == 1) {
-      chattingOver = true;
-      chatRoomDisappearingTime = du;
-      setState(() {});
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,8 +30,8 @@ class _ChatRoomGeneratedState extends State<ChatRoomGenerated> {
         context.push(
           '${CustomRouter.chatPath}/${CustomRouter.inChatPath}',
           extra: {
-            'id': widget.model.id,
-            'title': widget.model.chatTitle,
+            'id': model.id,
+            'title': model.chatTitle,
             'isGenerated': true,
           },
         );
@@ -112,25 +79,24 @@ class _ChatRoomGeneratedState extends State<ChatRoomGenerated> {
       ],
       child: Column(
         children: [
-          ChatRoomTop(model: widget.model),
+          ChatRoomTop(model: model),
           SizedBox(height: 10 * hu),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               ChatRoomProfiles(
-                profileCount: widget.model.profileThumbnails.length > 4
+                profileCount: model.profileThumbnails.length > 4
                     ? 4
-                    : widget.model.profileThumbnails.length,
-                profileSize:
-                    widget.model.profileThumbnails.length < 2 ? 45.0 : 30.0,
+                    : model.profileThumbnails.length,
+                profileSize: model.profileThumbnails.length < 2 ? 45.0 : 30.0,
               ),
               ChatRoomInfo(
-                date: widget.model.date,
-                location: widget.model.location,
-                chatTitle: widget.model.chatTitle,
-                latestMsg: widget.model.latestMsg!,
-                latestTime: widget.model.latestTime!,
-                notReadMsg: widget.model.notReadMsg!,
+                date: model.date,
+                location: model.location,
+                chatTitle: model.chatTitle,
+                latestMsg: model.latestMsg!,
+                latestTime: model.latestTime!,
+                notReadMsg: model.notReadMsg!,
               ),
             ],
           )
@@ -138,4 +104,12 @@ class _ChatRoomGeneratedState extends State<ChatRoomGenerated> {
       ),
     );
   }
+}
+
+void _alarmOnOffAction(bool withSlide) {
+  log("DmAction");
+}
+
+void _chatRoomOutAction(bool withSlide) {
+  log("DeleteAction");
 }
--- lib/ui/chat/widget/chat_room_info.dart
+++ lib/ui/chat/widget/chat_room_info.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:sottie_flutter/core/constant/custom_colors.dart';
 import 'package:sottie_flutter/ui/common/controller/screen_size.dart';
+import 'package:sottie_flutter/ui/common/controller/show_custom_dialog.dart';
 import 'package:sottie_flutter/ui/common/controller/ui_util.dart';
 
-class ChatRoomInfo extends StatelessWidget {
+class ChatRoomInfo extends StatefulWidget {
   const ChatRoomInfo({
     super.key,
     required this.date,
@@ -22,9 +26,38 @@ class ChatRoomInfo extends StatelessWidget {
   final int notReadMsg;
 
   @override
+  State<ChatRoomInfo> createState() => _ChatRoomInfoState();
+}
+
+class _ChatRoomInfoState extends State<ChatRoomInfo> {
+  bool chattingOver = false;
+  Duration? chatRoomDisappearingTime;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// 채팅 모임 날짜 기준 24시간 경과 후 채팅방이 사라지기 시작함. 24시간 후 완전히 사라짐.
+    /// 모임 날짜 + 24시간 까지 채팅 가능, 그 이후 24시간 채팅방 삭제 대기
+    /// inDays == 1은 시간 차이가 24시간 이상 48시간 미만을 의미
+    final generatedDate = DateTime.parse(widget.date);
+
+    /// utc로 변환해주지 않으면 한국 시간과 utc 시간으로 비교가 되어 정확한 시간 차이를 계산할 수 없다.
+    final now = DateTime.now().toUtc();
+    final du = now.difference(generatedDate);
+    log(du.inHours.toString());
+    if (du.inDays == 1) {
+      chattingOver = true;
+      chatRoomDisappearingTime = du;
+      setState(() {});
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final numOfMsg = notReadMsg > 999 ? '999+' : notReadMsg.toString();
-    final date = DateTime.parse(this.date);
+    final numOfMsg =
+        widget.notReadMsg > 999 ? '999+' : widget.notReadMsg.toString();
+    final date = DateTime.parse(widget.date);
 
     return SizedBox(
       width: 220 * wu,
@@ -36,7 +69,7 @@ class ChatRoomInfo extends StatelessWidget {
             width: 200 * wu,
             child: Text(
               overflow: TextOverflow.ellipsis,
-              chatTitle,
+              widget.chatTitle,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 16 * hu,
@@ -44,79 +77,202 @@ class ChatRoomInfo extends StatelessWidget {
             ),
           ),
           SizedBox(height: 3 * hu),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                width: 150 * wu,
-                child: Text(
+          chattingOver
+              ? Text(
+                  "채팅이 종료되었습니다",
                   overflow: TextOverflow.ellipsis,
-                  latestMsg,
                   style: TextStyle(
                     fontSize: 10 * hu,
                     fontWeight: FontWeight.bold,
-                    color: const Color(0x84000000),
+                    color: Colors.black.withOpacity(0.5),
                   ),
-                ),
-              ),
-              SizedBox(
-                width: 50 * wu,
-                child: Text(
-                  renderCustomStringTime(
-                      latestTime, DateTime.now().toUtc().toIso8601String()),
-                  style: TextStyle(
-                    fontSize: 8 * hu,
-                    color: const Color(0x64000000),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 3 * hu),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${date.year}년 ${date.month}월 ${date.day}일 ${renderCustomStringTime(this.date, this.date)}",
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    overflow: TextOverflow.ellipsis,
-                    location,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              if (notReadMsg != 0)
-                Padding(
-                  padding: EdgeInsets.only(right: 12 * wu),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.redAccent.withOpacity(0.8),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    width: 40 * wu,
-                    height: 20 * hu,
-                    padding: EdgeInsets.all(3 * hu),
-                    child: FittedBox(
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 150 * wu,
                       child: Text(
-                        numOfMsg,
-                        style: const TextStyle(
-                          color: mainSilverColor,
+                        overflow: TextOverflow.ellipsis,
+                        widget.latestMsg,
+                        style: TextStyle(
+                          fontSize: 10 * hu,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 50 * wu,
+                      child: Text(
+                        renderCustomStringTime(widget.latestTime,
+                            DateTime.now().toUtc().toIso8601String()),
+                        style: TextStyle(
+                          fontSize: 8 * hu,
+                          color: Colors.black.withOpacity(0.5),
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-            ],
-          ),
+          SizedBox(height: 3 * hu),
+          chattingOver
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "채팅방이 자동으로 삭제됩니다",
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 10 * wu,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        showCustomDialog(
+                          context,
+                          extraButton: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: const Size(100, 50),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                "확인",
+                                style: TextStyle(
+                                  color: mainSilverColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )),
+                          const Center(
+                            child: Text("채팅방을 나가시겠습니까?"),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.redAccent.withOpacity(0.7),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: EdgeInsets.all(5 * wu),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            _ChatRoomDestroyingTimer(
+                              timeLeft: chatRoomDisappearingTime ??
+                                  const Duration(hours: 23),
+                            ),
+                            const Text(
+                              "채팅방 나가기",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: mainSilverColor),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${date.year}년 ${date.month}월 ${date.day}일 ${renderCustomStringTime(widget.date, widget.date)}",
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          overflow: TextOverflow.ellipsis,
+                          widget.location,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    if (widget.notReadMsg != 0)
+                      Padding(
+                        padding: EdgeInsets.only(right: 12 * wu),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.redAccent.withOpacity(0.8),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          width: 40 * wu,
+                          height: 20 * hu,
+                          padding: EdgeInsets.all(3 * hu),
+                          child: FittedBox(
+                            child: Text(
+                              numOfMsg,
+                              style: const TextStyle(
+                                color: mainSilverColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
         ],
+      ),
+    );
+  }
+}
+
+class _ChatRoomDestroyingTimer extends StatefulWidget {
+  const _ChatRoomDestroyingTimer({required this.timeLeft});
+
+  final Duration timeLeft;
+
+  @override
+  State<_ChatRoomDestroyingTimer> createState() =>
+      _ChatRoomDestroyingTimerState();
+}
+
+class _ChatRoomDestroyingTimerState extends State<_ChatRoomDestroyingTimer> {
+  Duration timeLeftNow = const Duration();
+
+  String formatDuration(Duration d) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(d.inHours);
+    final minutes = twoDigits(d.inMinutes.remainder(60));
+    final seconds = twoDigits(d.inSeconds.remainder(60));
+    return '$hours:$minutes:$seconds';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    timeLeftNow = widget.timeLeft;
+
+    /// 채팅 종료 24시간 후 ~ 48시간 후 까지 이므로 48시간(172800초)에서 (현재 날짜 - 모임 날짜)(timeLeft)를 빼야 한다.
+    int seconds = 172800 - timeLeftNow.inSeconds;
+    timeLeftNow = Duration(seconds: seconds);
+    setState(() {});
+
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        seconds -= 1;
+        if (seconds < 0) {
+          timer.cancel();
+        } else {
+          timeLeftNow = Duration(seconds: seconds);
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      formatDuration(timeLeftNow),
+      style: const TextStyle(
+        fontWeight: FontWeight.bold,
       ),
     );
   }
