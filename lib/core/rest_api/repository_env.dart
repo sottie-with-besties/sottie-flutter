f1de5a81378dfa1fa09d77450ea589478e45c84a
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2025-01-26 17:26:24 +0900

    production repository 생성

--- lib/core/rest_api/repository_env.dart
+++ lib/core/rest_api/repository_env.dart
@@ -1,8 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:sottie_flutter/core/rest_api/dio_interceptor.dart';
 import 'package:sottie_flutter/data/auth/repository_impl/auth_dev_repository_impl.dart';
+import 'package:sottie_flutter/data/auth/repository_impl/auth_production_repository_impl.dart';
 import 'package:sottie_flutter/data/friend/repository_impl/friend_dev_repository_impl.dart';
+import 'package:sottie_flutter/data/friend/repository_impl/friend_production_repository_impl.dart';
 import 'package:sottie_flutter/data/post/repository_impl/post_dev_repository_impl.dart';
+import 'package:sottie_flutter/data/post/repository_impl/post_production_repository_impl.dart';
 import 'package:sottie_flutter/domain/auth/repository_interface/auth_repository.dart';
 import 'package:sottie_flutter/domain/friend/repository_interface/friend_repository.dart';
 import 'package:sottie_flutter/domain/post/repository_interface/post_repository.dart';
@@ -21,16 +24,26 @@ void initRepositories(RepositoryEnvironment repoEnv) {
       _initDevRepositories();
       break;
     case RepositoryEnvironment.production:
-      _initDevRepositories();
+      _initProductionRepositories();
       break;
   }
 }
 
-/// 개발 서버 이용
+/// 개발 서버 세팅
 void _initDevRepositories() {
   GetIt.I
       .registerSingleton<AuthRepository>(AuthTokenDevRepositoryImpl(cleanDio));
   GetIt.I.registerSingleton<PostRepository>(PostDevRepositoryImpl(cleanDio));
   GetIt.I
       .registerSingleton<FriendRepository>(FriendDevRepositoryImpl(cleanDio));
+}
+
+/// 프로덕션 서버 세팅
+void _initProductionRepositories() {
+  GetIt.I.registerSingleton<AuthRepository>(
+      AuthTokenProductionRepositoryImpl(cleanDio));
+  GetIt.I.registerSingleton<PostRepository>(
+      PostProductionRepositoryImpl(cleanDio));
+  GetIt.I.registerSingleton<FriendRepository>(
+      FriendProductionRepositoryImpl(cleanDio));
 }
---
+++ lib/data/friend/repository_impl/friend_production_repository_impl.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/retrofit.dart';
+import 'package:sottie_flutter/core/rest_api/repository_env.dart';
+import 'package:sottie_flutter/data/user/model/user_model.dart';
+import 'package:sottie_flutter/domain/friend/repository_interface/friend_repository.dart';
+
+part 'friend_production_repository_impl.g.dart';
+
+@RestApi(baseUrl: productionServerIp)
+abstract class FriendProductionRepositoryImpl implements FriendRepository {
+  factory FriendProductionRepositoryImpl(Dio dio, {String baseUrl}) =
+      _FriendProductionRepositoryImpl;
+
+  @override
+  @GET('/sottie/friends')
+  @Headers(<String, dynamic>{
+    'Content-Type': 'application/json',
+  })
+  Future<List<UserModel>> getFriends();
+}
