81396b962ee1918a9f5e13bfb1c3860878c1ab30
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2025-06-28 15:34:03 +0900

    Delete friend_model.dart

--- lib/model/friend/friend_model.dart
+++
@@ -1,7 +0,0 @@
-import 'package:sottie_flutter/model/user/user_model.dart';
-
-final class FriendListModel {
-  final List<UserModel> friendList;
-
-  FriendListModel({required this.friendList});
-}
